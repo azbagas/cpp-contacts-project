a85132cc838b87fd97aeda269c272f2f3932aee3
azbagas <azharbagaskara1@gmail.com>
2023-04-27 14:11:58 +0700

    tambah fitur baru dan rapih-rapih

--- main.cpp
+++ main.cpp
@@ -7,20 +7,103 @@
 using namespace std;
 
 int banyak_kontak;
-
 struct Data {
     string nama;
     string domisili;
     string nomor;
 };
 
-void handlePil() {
-    cout << "Harap masukkan pilihan yang benar!" << endl;
+// Functions
+void header(string text);
+void pressAnyKey();
+void handlePil();
+void tabel(Data kontak[]);
+void tampilanTabel(Data kontak[]);
+void tampilanDetail(Data kontak[]);
+void menuTampilan(Data kontak[]);
+void printDetail(Data kontak[], int i);
+void sorting(Data kontak[]);
+void search(Data kontak[]);
+void read(string str, char del, Data kontak[], int i);
+void inisialisasiKontak(Data kontak[]);
+void titleCase(string &str);
+void tambahKontak(Data kontak[]);
+void aboutUs();
+void hapusKontak(Data kontak[]);
+void editKontak(Data kontak[]);
+int banyakLine();
+
+// Main
+int main() {
+    int pil;
+    
+    Data kontak[MAX];
+    inisialisasiKontak(kontak);
+
+    do {
+        system("cls");
+        header("My Contacts");
+        cout << "1. Tampilkan data kontak" << endl;
+        cout << "2. Tambahkan kontak" << endl;
+        cout << "3. Cari kontak" << endl;
+        cout << "4. Hapus kontak" << endl;
+        cout << "5. Edit kontak" << endl;
+        cout << "6. About Us" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Pilih: ";
+        cin >> pil;
+        
+        system("cls");
+
+        switch (pil) {
+        case 1:
+            menuTampilan(kontak);
+            break;
+        case 2:
+            tambahKontak(kontak);
+            break;
+        case 3:
+            search(kontak);
+            break;
+        case 4:
+            hapusKontak(kontak);
+            break;
+        case 5:
+            editKontak(kontak);
+            break;
+        case 6:
+            aboutUs();
+        case 0:
+            cout << "Thank you..." << endl;
+            break;
+        default:
+            handlePil();
+            break;
+        }
+
+    } while (pil != 0);
+    
+    return 0;
+}
+
+void header(string text) {
+    cout << "==========================" << endl;
+    cout << "       " << text << endl;
+    cout << "==========================" << endl;
+}
+
+void pressAnyKey() {
     cout << "Press any key to continue...";
     getch();
 }
 
+void handlePil() {
+    cout << "Harap masukkan pilihan yang benar!" << endl;
+    pressAnyKey();
+}
+
 int banyakLine() {
+    // Pram
     string line;
     int count = 0;
 
@@ -39,28 +122,31 @@ int banyakLine() {
     return count;
 }
 
+void tabel(Data kontak[]) {
+    cout<<"===========================================================================================================\n";
+    cout<<"|  No  |           Nama           |     No Telepon    |                       Alamat                      |\n";
+    cout<<"===========================================================================================================\n";
+    for (int i = 0; i < banyak_kontak; i++){
+        cout << "| " << setiosflags(ios::left) << setw(5) << i+1 << "|";
+        cout << " " << setiosflags(ios::left) << setw(25) << kontak[i].nama << "|";
+        cout << " " << setiosflags(ios::left) << setw(18) << kontak[i].nomor << "|";
+        cout << " " << setiosflags(ios::left) << setw(50) << kontak[i].domisili << "|";
+        cout <<"\n";
+    }
+    cout << "-----------------------------------------------------------------------------------------------------------\n\n";
+}
+
 void tampilanTabel(Data kontak[]) {
+    // Syifa
     if (banyak_kontak == 0) {
         cout << "Tidak ada kontak untuk ditampilkan!" << endl;
     }
     else {
-        cout << "==========================" << endl;
-        cout << "       Daftar Kontak      " << endl;
-        cout << "==========================" << endl;
-        cout<<"===========================================================================================================\n";
-        cout<<"|  No  |           Nama           |     No Telepon    |                       Alamat                      |\n";
-        cout<<"===========================================================================================================\n";
-        for (int i = 0; i < banyak_kontak; i++){
-            cout << "| " << setiosflags(ios::left) << setw(5) << i+1 << "|";
-            cout << " " << setiosflags(ios::left) << setw(25) << kontak[i].nama << "|";
-            cout << " " << setiosflags(ios::left) << setw(18) << kontak[i].nomor << "|";
-            cout << " " << setiosflags(ios::left) << setw(50) << kontak[i].domisili << "|";
-            cout <<"\n-----------------------------------------------------------------------------------------------------------\n";
-        }
+        header("Daftar Kontak");
+        tabel(kontak);
     }
 
-    cout << "Press any key to continue...";
-    getch();
+    pressAnyKey();
 }
 
 void tampilanDetail(Data kontak[]) {
@@ -68,14 +154,11 @@ void tampilanDetail(Data kontak[]) {
     int pil;
     if (banyak_kontak == 0) {
         cout << "Tidak ada kontak untuk ditampilkan!" << endl;
-        cout << "Press any key to continue...";
-        getch();
+        pressAnyKey();
     }
     else {
         do {
-            cout << "==========================" << endl; 
-            cout << "       Detail Kontak      " << endl;
-            cout << "==========================" << endl;
+            header("Detail Kontak");
             cout << "Nomor (" << i+1 << "/"<< banyak_kontak << ")" << endl;
             cout << "Nama          : " << kontak[i].nama << endl;
             cout << "Nomor Telepon : " << kontak[i].nomor << endl;
@@ -116,9 +199,7 @@ void tampilanDetail(Data kontak[]) {
 
 void menuTampilan(Data kontak[]) {
     int pil;
-    cout << "==========================" << endl; 
-    cout << "        Data Kontak       " << endl;
-    cout << "==========================" << endl;
+    header("Data Kontak");
     cout << "1. Tampilkan tabel" << endl;
     cout << "2. Tampilkan detail" << endl;
     cout << "0. Kembali" << endl;
@@ -162,12 +243,11 @@ void sorting(Data kontak[]) {
 }
 
 void search(Data kontak[]) {
+    // Marsha
     bool flag = false;
     int i;
     string key;
-    cout << "==========================" << endl;
-    cout << "        Cari Kontak      " << endl;
-    cout << "==========================" << endl;
+    header("Cari Kontak");
     cout << "Nama: "; cin >> ws; getline(cin, key);
 
     for (i = 0; i < banyak_kontak; i++) {
@@ -185,8 +265,7 @@ void search(Data kontak[]) {
     }
     
     cout << endl;
-    cout << "Press any key to continue...";
-    getch();
+    pressAnyKey();
 }
 
 void read(string str, char del, Data kontak[], int i){
@@ -217,6 +296,7 @@ void read(string str, char del, Data kontak[], int i){
 }
 
 void inisialisasiKontak(Data kontak[]) {
+    // Faza
     banyak_kontak = banyakLine();
     string line;
     ifstream file("data.txt");
@@ -247,9 +327,7 @@ void titleCase(string &str) {
 }
 
 void tambahKontak(Data kontak[]) {
-    cout << "==========================" << endl;
-    cout << "       Tambah Kontak      " << endl;
-    cout << "==========================" << endl;
+    header("Tambah Kontak");
     cout << "Nama           : "; cin >> ws; getline(cin, kontak[banyak_kontak].nama);
     cout << "Nomor Telepon  : "; getline(cin, kontak[banyak_kontak].nomor);
     cout << "Domisili       : "; getline(cin, kontak[banyak_kontak].domisili);
@@ -263,6 +341,7 @@ void tambahKontak(Data kontak[]) {
     // Lalu sorting alfabetikal
     sorting(kontak);
 
+    // Pasha
     // Masukkin datanya ke file
     ofstream file("data.txt", ios::out);
     if(file.is_open()) {
@@ -281,50 +360,118 @@ void tambahKontak(Data kontak[]) {
 
     cout << endl;
     cout << "Data telah berhasil diinput." << endl;
-    cout << "Press any key to continue...";
-    getch();
- 
+    pressAnyKey();
 }
 
-int main() {
-    int pil;
-    
-    Data kontak[MAX];
-    inisialisasiKontak(kontak);
+void aboutUs() {
+    header("About Us");
+    cout << "Project By:" << endl;
+    cout << "- Azhar Bagaskara (22/502652/PA/21573)" << endl;
+    cout << "- Faza Aster Apriliani (22/500393/PA/21550)" << endl;
+    cout << "- Marsha Rasyida Al-Farabi (22/499914/PA/21544)" << endl;
+    cout << "- Pasha Rai Azi Zaydan (22/505428/PA/21765)" << endl;
+    cout << "- Pramudya Putra Hidayat (22/496600/PA/21349)" << endl;
+    cout << "- Syifa Ramadhanty (22/504446/PA/21699)" << endl << endl;
+    pressAnyKey();
+}
 
-    do {
-        system("cls");
-        cout << "==========================" << endl; 
-        cout << "        My Contacts       " << endl;
-        cout << "==========================" << endl;
-        cout << "1. Tampilkan data kontak" << endl;
-        cout << "2. Tambahkan kontak" << endl;
-        cout << "3. Cari kontak" << endl;
-        cout << "0. Keluar" << endl;
-        cout << "Pilih: ";
-        cin >> pil;
-        
-        system("cls");
+void hapusKontak(Data kontak[]) {
+    int nomor;
+    char pil;
+    header("Hapus Kontak");
+    tabel(kontak);
+    cout << "Masukkan nomor berapa yang ingin dihapus: ";
+    cin >> nomor;
+    cout << "Anda yakin ingin menghapus " << kontak[nomor-1].nama << "? [y/n]: ";
+    cin >> pil;
 
-        switch (pil) {
-        case 1:
-            menuTampilan(kontak);
-            break;
-        case 2:
-            tambahKontak(kontak);
-            break;
-        case 3:
-            search(kontak);
-            break;
-        case 0:
-            cout << "Thank you..." << endl;
-            break;
-        default:
-            handlePil();
-            break;
+    if (pil == 'y' || pil == 'Y') {
+        for (int i = nomor-1; i < banyak_kontak; i++) {
+            kontak[i] = kontak[i+1];
+        }
+        banyak_kontak--;
+
+        sorting(kontak);
+        // Masukkin datanya ke file
+        ofstream file("data.txt", ios::out);
+        if(file.is_open()) {
+            for (int i = 0; i < banyak_kontak; i++) {
+                if (i > 0) {
+                    file << endl;
+                }
+                file << kontak[i].nama << "#" << kontak[i].domisili << "#" << kontak[i].nomor;
+            }
+            
+            file.close();
+        }
+        else {
+            cout << "Tidak bisa membuka file";
         }
 
-    } while (pil != 0);
+        cout << "Kontak berhasil dihapus" << endl;
+        pressAnyKey();
+    }
+    else {
+        return;
+    }
+}
+
+void editKontak(Data kontak[]) {
+    int nomor;
+    char pil;
+    header("Edit Kontak");
+    tabel(kontak);
+    cout << "Masukkan angka 0 jika ingin kembali ke menu utama\n\n";
+    cout << "Masukkan nomor berapa yang ingin diedit: ";
+    cin >> nomor;
+    nomor--;
+
+    if (nomor == -1)
+        return;
     
-    return 0;
+    if (nomor < -1 || nomor >= banyak_kontak) {
+        cout << "Nomor tidak valid" << endl;
+        pressAnyKey();
+        return;
+    }
+
+    string temp;
+    cout << endl;
+    cout << "Masukkan strip (-) jika tidak ingin diubah" << endl;
+    cout << kontak[nomor].nama << " -> ";
+    cin >> ws;
+    getline(cin, temp);
+    if (temp != "-") {
+        kontak[nomor].nama = temp;
+    }
+    cout << kontak[nomor].domisili << " -> ";
+    getline(cin, temp);
+    if (temp != "-") {
+        kontak[nomor].domisili = temp;
+    }
+    cout << kontak[nomor].nomor << " -> ";
+    getline(cin, temp);
+    if (temp != "-") {
+        kontak[nomor].nomor = temp;
+    }
+
+    // Masukkin datanya ke file
+    ofstream file("data.txt", ios::out);
+    if(file.is_open()) {
+        for (int i = 0; i < banyak_kontak; i++) {
+            if (i > 0) {
+                file << endl;
+            }
+            file << kontak[i].nama << "#" << kontak[i].domisili << "#" << kontak[i].nomor;
+        }
+        
+        file.close();
+    }
+    else {
+        cout << "Tidak bisa membuka file";
+    }
+
+    cout << endl;
+    cout << "Kontak berhasil diedit" << endl;
+    pressAnyKey();
 }
